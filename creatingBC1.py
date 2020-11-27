ca6fd6107c497dedc7c70558cce433bd8e3afffc
Sandeep N <sandeepbk01@gmail.com>
2020-11-27 15:40:07 +0530

    Added Mining and tested in postman

--- creatingBC1.py
+++ creatingBC1.py
@@ -58,9 +58,53 @@ class Blockchain:
                 return False
             previous_block = block
             block_index+=1
-        return True  #Hence returned
+        return True  
 
 
-        #Mining now!.......
+#Mining the blockChain
+
+#Creating a web app
+
+app = Flask(__name__)
+
+#Creating blockchain
+
+blockchain = Blockchain()
+
+#Mining new blockchain
+
+@app.route('/mine_block',methods = ['GET'])
+
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof,previous_hash)
+    response = {'message':'Congratulations , you mined a block',
+                  'index':block['index'],
+                  'timestamp' : block['timestamp'],
+                  'proof' : block['proof'],
+                  'previous_hash' : block['previous_hash']  }
+
+    return jsonify(response),200
+
+@app.route('/get_chain',methods = ['GET'])
+
+def get_chain():
+    response = {'chain': blockchain.chain,
+                 'length' : len(blockchain.chain)   }
+
+    return jsonify(response), 200
+
+#Running 
+
+app.run(host = '0.0.0.0',port = 5000)
+
+
+
+
+
+        
 
 
